f65015fc5da1018b5d5985a816c646372642f53e
Xuwen <xuwen@ion.one>
2018-12-27 19:26:26 +0800

    e0 cfg

--- start-e0.sh
+++ start-e0.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 cd /home/ubuntu/stellar-core
 sleep 30
-/usr/local/bin/stellar-core --forcescp --conf cfgs/stellar-core-e1.cfg
-/usr/local/bin/stellar-core stellar-core --conf cfgs/stellar-core-e1.cfg
+/usr/local/bin/stellar-core --forcescp --conf cfgs/stellar-core-e0.cfg
+/usr/local/bin/stellar-core stellar-core --conf cfgs/stellar-core-e0.cfg
 echo "stellar service started"
