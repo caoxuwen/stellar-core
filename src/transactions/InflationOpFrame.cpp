bc099e76dcfa247b16c6e19875def1a844edb90e
Xuwen <xuwen@ion.one>
2018-12-31 21:48:24 +0800

    adjust inflation process

--- src/transactions/InflationOpFrame.cpp
+++ src/transactions/InflationOpFrame.cpp
@@ -19,7 +19,7 @@
 #include "xdr/Stellar-ledger-entries.h"
 #include <list>
 
-const uint64_t FUNDING_INTERVAL = (60 * 60);     // every hour
+const uint64_t FUNDING_INTERVAL = (60 * 60);      // every hour
 const stellar::int64 BASE_CONVERSION = 10000000; // 10^7
 const stellar::int64 DEPTH_THRESHOLD = 100 * BASE_CONVERSION;
 const double DIFF_THRESHOLD = 0.005;
@@ -55,13 +55,6 @@ InflationOpFrame::doApply(Application& app, AbstractLedgerState& ls)
     for (auto const& tradingPair : app.getConfig().TRADING)
     {
         TradingConfiguration config = tradingPair.second;
-
-        /*
-        CLOG(DEBUG, "Tx") << config.mName;
-        CLOG(DEBUG, "Tx") << config.mCoin1[0];
-        CLOG(DEBUG, "Tx") << config.mCoin2[0];
-        CLOG(DEBUG, "Tx") << config.mBaseAsset[0];
-        CLOG(DEBUG, "Tx") << config.mReferenceFeed[0];*/
 
         CLOG(DEBUG, "Tx") << config.mName;
         CLOG(DEBUG, "Tx") << config.mCoin1.mName << " "
@@ -118,6 +111,9 @@ InflationOpFrame::doApply(Application& app, AbstractLedgerState& ls)
 
         CLOG(DEBUG, "Tx") << "midPrice " << midOrderbookPrice;
 
+        innerResult().code(INFLATION_SUCCESS);
+        lh.inflationSeq++;
+        lh.lastFunding = closeTime;
         // now credit each account
         auto& payouts = innerResult().payouts();
 
@@ -188,10 +184,6 @@ InflationOpFrame::doApply(Application& app, AbstractLedgerState& ls)
             }
         }
     }
-
-    innerResult().code(INFLATION_SUCCESS);
-    lh.inflationSeq++;
-    lh.lastFunding = closeTime;
 
     app.getMetrics()
         .NewMeter({"op-inflation", "success", "apply"}, "operation")
